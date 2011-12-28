f635d634fe57b64d04429dab8717e17972b00ae6
Dan Burkert <danburkert@gmail.com>
2011-12-27 18:48:21 -0600

    set up directory structure, add in eitherpad to page

---
+++ js/load-pad.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#pad").pad({
+                  'padId' : 'kRBXB8P1Ci',
+                  'host'  : 'http://notes.avaaz.org',
+                  'baseUrl' : '/'
+               });
+  $("#epframepad").css("width", "800px");
+  $("#epframepad").css("height", "800px");
+
+});
